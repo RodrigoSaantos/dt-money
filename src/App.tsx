d589f7915a1007706dbcc3e3436c5da0b6203d6b
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2022-08-21 21:21:39 -0300

    Component: Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "styled-components";
+import { Transactions } from "./pages/Transactions";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
@@ -7,7 +8,8 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <div>Hello world</div>
+      
+      <Transactions />
     </ThemeProvider>
   )
 }
